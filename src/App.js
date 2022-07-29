77ea33b8cae5c931cc54f8566e67ea6e10ebf6d8
brooks1114 <58515991+brooks1114@users.noreply.github.com>
2022-07-28 20:55:32 -0400

    Set up Complete. Added Shuffle Card Function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
+
+const cardImages = [
+  { "src": "/img/helmet-1.png" },
+  { "src": "/img/potion-1.png" },
+  { "src": "/img/ring-1.png" },
+  { "src": "/img/scroll-1.png" },
+  { "src": "/img/shield-1.png" },
+  { "src": "/img/sword-1.png" }
+
+]
 
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  //shuffle cards
+  // [...cardImages, ...cardImages] this syntax will add the card images array once, then again. so shuffleCards now has 2 copies of each card (member of the array cardImages)
+  // .sort will compare 2 items. if we return a number less than zero, the numbers remain the same, greater than 0 they swap.Math.random() returns a number between 0-1 by default.
+  // the map will allow us to add a random ID property to each card in the array of cards
+  const shuffleCards = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+    setCards(shuffleCards)
+    // We set this to 0 here because everytime we start a new game, shuffle cards is called and will reset the the counter to 0
+    setTurns(0)
+  }
+
+  console.group(cards, turns)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Match Game</h1>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
